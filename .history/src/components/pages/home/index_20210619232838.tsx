3f7f25f71634c86e015e59486838ffa34abc8ac3
Robert <majointa@gmail.com>
2021-06-19 23:36:35 +0200

    add hero page

--- .history/src/components/pages/home/index_20210619232838.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import Hero from "@components/layout/hero";
-
-const index = (): JSX.Element => {
-  return <Hero />;
-};
-
-export default index;
--- .history/src/components/pages/home/index_20210619232910.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import Hero from "@components/layout/hero";
-
-const Homepage = (): JSX.Element => {
-  return <Hero />;
-};
-
-export default Homepage;
