cd2fae568e4b1e7f3d5480525b9f2c6dfd6456a2
Robert <majointa@gmail.com>
2021-06-19 23:20:15 +0200

    fisrt commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
-function App() {
+import { ROUTE_PATHS } from "./config/route";
+import { NavMenu } from "./components/layout/nav";
+import { Footer } from "@components/layout/footer";
+
+import "bootstrap/dist/css/bootstrap.css";
+
+const App = (): JSX.Element => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Suspense fallback={"Loading"}>
+        <NavMenu />
+        <Switch>
+          {ROUTE_PATHS.map(({ exact, path, component }, index) => (
+            <Route {...{ path, component, exact, key: index }} />
+          ))}
+        </Switch>
+        <Footer />
+      </Suspense>
+    </Router>
   );
-}
+};
 
 export default App;
